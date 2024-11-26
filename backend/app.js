4f7e1149e66599d3b538ef519220c7d8c42203f5
chikkibum <bhaskarpandeycontacts@gmail.com>
2024-11-27 00:54:52 +0530

    user_model done

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+dotenv.config()
 const app = express()
+
+
+app.use(cors())
 
 app.get("/",(req,res) =>{
     res.send("hello")
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000; // Default to 3000 if undefined
+console.log(`PORT from .env: ${PORT}`);
+  
+console.log(PORT)
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports=app;
